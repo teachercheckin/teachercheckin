75270bb7bcf0d1c96efe782be2ef422ba4c7130b
teachercheckin <141989113+teachercheckin@users.noreply.github.com>
2023-08-20 14:42:38 -0700

    Update spreadsheetID.js

--- src/api/spreadsheetID.js
+++ src/api/spreadsheetID.js
@@ -1,2 +1,3 @@
 export const spreadsheetID = {production: "1sTfGfuB4Mx1GupbqCdj4v9uHYKX3j-Gfy8IDxcGcV2o",
-                              development: "12AWolV6lI99LM6NNP1bUwYanAuNDSWRJI8X4-ozM98Q"};
+                              development: "1sTfGfuB4Mx1GupbqCdj4v9uHYKX3j-Gfy8IDxcGcV2o"};
+//12AWolV6lI99LM6NNP1bUwYanAuNDSWRJI8X4-ozM98Q
